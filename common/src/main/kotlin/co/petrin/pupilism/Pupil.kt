86f9e9ad5147ff239c56133651314511c2b50e04
Gregor Petrin <gregap@gmail.com>
2018-01-07 20:00:00 +0100

    Store a pupil's activity IDs in their profile

--- common/src/main/kotlin/co/petrin/pupilism/Pupil.kt
+++ common/src/main/kotlin/co/petrin/pupilism/Pupil.kt
@@ -4,8 +4,9 @@ package co.petrin.pupilism
  * Represents a pupil.
  * @property name Name of the pupil
  * @property klass The klass/class this pupil is enrolled in
+ * @property activities A list of activity IDs this pupil is enrolled in
  * @property leaveTimes The known leave time of kids
  */
-class Pupil(val name: String, val klass: Klass, val leaveTimes: MutableMap<DayOfWeek, Time> = mutableMapOf()) {
+class Pupil(val name: String, val klass: Klass, val activities: List<String> = emptyList(), val leaveTimes: MutableMap<DayOfWeek, Time> = mutableMapOf()) {
     override fun toString() = "$name, $klass"
 }
